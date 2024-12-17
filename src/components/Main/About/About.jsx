3cfdbc66105d19ce91bfda4dcab861308e443032
Victor Garritano <victor@getupgraded.com>
2024-12-17 15:54:47 +0100

    avances

--- src/components/Main/About/About.jsx
+++ src/components/Main/About/About.jsx
@@ -11,10 +11,10 @@ const About = () => {
 
   return <>
   <section className="about">
-  <h1>About me</h1>
+  <h1 className="aboutTitle">About me</h1>
   <img className="profileImg" src="https://avatars.githubusercontent.com/u/146452699?v=4"></img>
   <article className="text">
-  <p>Hello! I'm Victor Garritano. I'm a Fullstack Developer, working really hard to hone my skills in both frontend and backend development. I love learning about new technologies and constantly improving my coding abilities. 🚀</p>
+  <p className="parrafo">Hello! I'm Victor Garritano. I'm a Fullstack Developer, working really hard to hone my skills in both frontend and backend development. I love learning about new technologies and constantly improving my coding abilities. 🚀</p>
 
   </article>
   </section>
